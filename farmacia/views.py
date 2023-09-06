7d0a48f80f179b8c97f935cbae90c4fb2641bca2
Verónica Marín <veronica.marin@catolica.edu.sv>
2023-09-06 12:52:05 -0600

    Agregando formulario de inicio se sesion

--- farmacia/views.py
+++ farmacia/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
 
 def inicioAdmin(request):
     return render(request, 'inicioAdmin.html',{})
 def inicio(request):
     return render(request, 'inicio.html',{})
 def login(request):
-    return render(request, 'login.html',{})
+    return render(request, 'login.html',{
+        'form': AuthenticationForm
+    })
