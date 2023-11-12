d36b720d355e9cb8d24049015c484a60534363d6
Verónica Marín <veronica.marin@catolica.edu.sv>
2023-11-11 19:19:20 -0600

    Cambio

--- inventario/forms.py
+++ inventario/forms.py
@@ -69,8 +69,9 @@ class LoteForm(forms.ModelForm):
             if precio_compra > idarticulo.precio_venta:
                 self.add_error('precio_compra', 'El precio de compra no puede ser mayor al precio de venta')
 
-        if lote and self.Meta.model.objects.filter(idarticulo=idarticulo, lote=lote).exists():
+        if self.instance.pk is None and lote and self.Meta.model.objects.filter(idarticulo=idarticulo, lote=lote).exists():
             self.add_error('lote', 'Ya hay un producto asignado con este lote')
+
 
     class Meta:
         model = lote
